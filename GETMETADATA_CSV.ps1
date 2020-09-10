cd36607eee694537c17b187ec064a8a57e469774
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2020-09-10 14:29:58 -0300

    Add files via upload

---
+++ GETMETADATA_CSV.ps1
@@ -0,0 +1,25 @@
+﻿
+# To execute the script without agreeing with the execution policy
+Set-ExecutionPolicy Bypass -Scope Process
+
+# Defines the directory where the PS1 file will be, which has the function to discover the file metadata
+cd C:\
+
+# Load the PS1 file
+.\Get-FileMetaDataReturnObject.ps1
+
+# Import the PS1 file to access the functions
+Import-module .\Get-FileMetaDataReturnObject.ps1 -Force
+
+# Defines the directory where the files will be, to get their metadata
+$FilePath = "C:\"
+
+# Defines the directory and name of the file to be exported to the CSV file
+$Dir = "C:\METADATA_CSV.csv"
+
+# Loads photo metadata into the variable
+$Metadata = Get-FileMetaData -folder $FilePath
+
+# Exports the metadata of the files found in the directory, to a CSV file
+$Metadata | 
+	Export-Csv -Path $Dir -NoTypeInformation -Encoding UTF8
---
+++ Get-FileMetaDataReturnObject.ps1
@@ -0,0 +1,80 @@
+﻿# -----------------------------------------------------------------------------
+# Script: Get-FileMetaDataReturnObject.ps1
+# Author: ed wilson, msft
+# Date: 01/24/2014 12:30:18
+# Keywords: Metadata, Storage, Files
+# comments: Uses the Shell.APplication object to get file metadata
+# Gets all the metadata and returns a custom PSObject
+# it is a bit slow right now, because I need to check all 266 fields
+# for each file, and then create a custom object and emit it.
+# If used, use a variable to store the returned objects before attempting
+# to do any sorting, filtering, and formatting of the output.
+# To do a recursive lookup of all metadata on all files, use this type
+# of syntax to call the function:
+# Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName
+# note: this MUST point to a folder, and not to a file.
+# -----------------------------------------------------------------------------
+Function Get-FileMetaData
+{
+  <#
+   .Synopsis
+    This function gets file metadata and returns it as a custom PS Object 
+   .Description
+    This function gets file metadata using the Shell.Application object and
+    returns a custom PSObject object that can be sorted, filtered or otherwise
+    manipulated.
+   .Example
+    Get-FileMetaData -folder "e:\music"
+    Gets file metadata for all files in the e:\music directory
+   .Example
+    Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName
+    This example uses the Get-ChildItem cmdlet to do a recursive lookup of 
+    all directories in the e:\music folder and then it goes through and gets
+    all of the file metada for all the files in the directories and in the 
+    subdirectories.  
+   .Example
+    Get-FileMetaData -folder "c:\fso","E:\music\Big Boi"
+    Gets file metadata from files in both the c:\fso directory and the
+    e:\music\big boi directory.
+   .Example
+    $meta = Get-FileMetaData -folder "E:\music"
+    This example gets file metadata from all files in the root of the
+    e:\music directory and stores the returned custom objects in a $meta 
+    variable for later processing and manipulation.
+   .Parameter Folder
+    The folder that is parsed for files 
+   .Notes
+    NAME:  Get-FileMetaData
+    AUTHOR: ed wilson, msft
+    LASTEDIT: 01/24/2014 14:08:24
+    KEYWORDS: Storage, Files, Metadata
+    HSG: HSG-2-5-14
+   .Link
+     Http://www.ScriptingGuys.com
+ #Requires -Version 2.0
+ #>
+ Param([string[]]$folder)
+ foreach($sFolder in $folder)
+  {
+   $a = 0
+   $objShell = New-Object -ComObject Shell.Application
+   $objFolder = $objShell.namespace($sFolder)
+
+   foreach ($File in $objFolder.items())
+    { 
+     $FileMetaData = New-Object PSOBJECT
+      for ($a ; $a  -le 266; $a++)
+       { 
+         if($objFolder.getDetailsOf($File, $a))
+           {
+             $hash += @{$($objFolder.getDetailsOf($objFolder.items, $a))  =
+                   $($objFolder.getDetailsOf($File, $a)) }
+            $FileMetaData | Add-Member $hash
+            $hash.clear() 
+           } #end if
+       } #end for 
+     $a=0
+     $FileMetaData
+    } #end foreach $file
+  } #end foreach $sfolder
+} #end Get-FileMetaData
